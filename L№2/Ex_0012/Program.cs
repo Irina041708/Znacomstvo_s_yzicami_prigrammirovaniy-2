704688162b5298efead7b826bf28c3b000ea9f54
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-13 01:32:09 +0400

    L№2

---
+++ L№2/Ex_0012/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {2,3,6,5,4,4,8,9,4};
+int n = array.Length;
+int find = 4;
+
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+       Console.WriteLine(index); 
+       break;
+    }
+    index++;
+}
---
+++ L№2/Ex_008_2Lekciy_1_Zadanie/Program.cs
@@ -0,0 +1,26 @@
+﻿int a1 = 15;
+int b1 = 25;
+int c1 = 35;
+
+int a2 = 45;
+int b2 = 163;
+int c2 = 65;
+
+int a3 = 75;
+int b3 = 85;
+int c3 = 3;
+
+int max = a1;
+
+if (max < b1) max = b1;
+if (max < c1) max = c1;
+
+if (max < a2) max = a2;
+if (max < b2) max = b2;
+if (max < c2) max = c2;
+
+if (max < a3) max = a3;
+if (max < b3) max = b3;
+if (max < c3) max = c3;
+
+Console.WriteLine (max);
