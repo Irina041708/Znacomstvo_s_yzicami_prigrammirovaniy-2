8c9d6a62f3d0ed91139411561582727c328fccc4
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-13 01:31:57 +0400

    L№1

---
+++ L№1/Ex_005/Program.cs
@@ -0,0 +1,6 @@
+﻿double numberA = new Random().Next(0,10); //0,1...9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(0,10); //0,1...9
+Console.WriteLine(numberB);
+double result = numberA+numberB;
+Console.WriteLine(result);
