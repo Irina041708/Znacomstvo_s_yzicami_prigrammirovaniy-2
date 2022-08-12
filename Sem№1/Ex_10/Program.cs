44c480d698ef0b8e681bf31f0953753bdf30fca5
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-13 01:32:37 +0400

    Sem№1

---
+++ Sem№1/Ex_10/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Ведите первое число");
+string A = Console.ReadLine()??"";
+int a;
+Console.WriteLine("Ведите второе число");
+string B = Console.ReadLine()??"";
+int b;
+if ((int.TryParse(A, out a)) & (int.TryParse(B, out b)))
+    {
+        if (a == b*b) 
+            {
+                Console.Write(a); 
+                Console.Write(" являетя квадратом ");  
+                Console.WriteLine(b); 
+            }
+    
+        else 
+            {
+                Console.Write(a); 
+                Console.Write(" не являетя квадратом ");  
+                Console.WriteLine(b);
+            }
+    }
+else 
+    {
+    Console.Write(a); 
+    Console.Write("не являетя квадратом ");  
+    Console.WriteLine(b);
+    }
+    
