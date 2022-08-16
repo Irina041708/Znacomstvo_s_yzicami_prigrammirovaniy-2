914dd4d6bc2f658c394c94b21fa069564c9b46aa
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-17 01:58:20 +0400

    Sem№4

---
+++ Sem№4/Zad№1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int number;
+Console.Write("ВВедите любое число: ");
+number = int.Parse(Console.ReadLine()?? "" );
+number = countNumber (number); 
+Console.Write ($"Число состоит из {number} цифр");
+
+int countNumber (int num) 
+{
+    int i = 0;
+    while (num > 0)
+    {
+        num/=10;
+        i++;
+    }
+    return i;
+}
---
+++ Sem№4/Zad№2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("ВВедите любое число: ");
+int userNumber = int.Parse(Console.ReadLine()?? "" );
+int factorial = calculatorFactorial (userNumber); 
+Console.Write ($"Факториал числа {userNumber} = {factorial} ");
+
+int calculatorFactorial (int num) 
+{
+    int result = 1, i = 1;
+    while (i<=num)
+    {
+        result*=i;
+        i++;
+    }
+    return result;
+}
