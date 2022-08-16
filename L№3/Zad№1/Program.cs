cac384acd9471edf8f1dc6ce8c60f6a176956703
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-17 01:59:09 +0400

    L№3

--- L№3/Zad№1/Program.cs
+++ L№3/Zad№1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+String text = "тарам памапам - = -ю"
+                +"тарам "
+                +"Люлякебаб .";
+string Replace (string text, char oldValue, char newValue)
+{
+    string result = String.Empty; // инициализация пустой строки
+
+    int Length = text.Length;
+    for (int i = 0; i < text.Length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+
+}
+string newText = Replace (text, ' ' ,  '|' );
+Console.WriteLine(newText);
