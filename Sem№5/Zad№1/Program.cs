576019450a12ae9957997f2c210b7139d36fc16d
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-22 04:18:00 +0400

    Sem№5

---
+++ Sem№5/Zad№1/Program.cs
@@ -0,0 +1,54 @@
+﻿/*31. Задайте массив из 12 элементов, заполенный числами из промежутка из [-9,9].
+Найдите сумму отрицательных и положителных чисел.*/
+
+
+Console.Clear();
+
+int [] array = GetArray (12,-9,9);
+Console.WriteLine(String.Join(" ", array));
+
+int positiveSum = GetpositiveSum(array);//сумма положительных числе
+int negativeSum = GetNegativeSum(array);//сумма отрицательных чисел
+
+Console.WriteLine ($"Positive Sum = {positiveSum}, Negative Sum = {negativeSum}");
+
+///////////////////////////////////////////////////////////////////////////////////
+//Возвращает массив из size элементов,
+//заполненный случайными числами из промежутка [minValue, maxValue].
+int [] GetArray (int size, int minValue, int maxValue)
+{
+    int [] res = new int [size];// Создаем в памяти массив целых чисел с размером size
+    for (int i=0; i < size ; i ++)
+    {
+        res [i] = new Random().Next (minValue, maxValue + 1);
+    }
+    return res;
+}
+
+
+// Возвращает сумму положительных чисел массива arr
+int GetpositiveSum(int[] arr)
+{
+    int positiveSum = 0;
+    foreach (int el in arr)
+    {
+        if (el > 0) positiveSum += el;
+        
+    }
+    return positiveSum;
+
+}
+
+// Возвращает сумму отрицательных чисел массива arr
+int GetNegativeSum (int[] arr)
+{
+    int negativeSum = 0;
+    foreach (int el in arr)
+    {
+        if (el > 0) negativeSum += el <0 ? el : 0 ;
+       
+    }
+    return positiveSum;
+
+}
+
---
+++ Sem№5/Zad№2/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 32: Напишите программу замена элементов массива: положительные элементы замените 
+на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]  */
+
+
+Console.Clear();
+
+Console.WriteLine($"Наш массив");
+int[] array = GetArray(4, -9, 9);
+
+Console.Write($"[ ");
+Console.Write($" {String.Join(" ", array)}");
+Console.WriteLine($"]");
+
+array = GetReplace (array);
+
+Console.Write($"[ ");
+Console.Write($" {String.Join(" ", array)}");
+Console.Write($"]");
+
+///////////////////////////////////////////////////////////////////////////////////
+
+//Возвращает массив из size элементов,
+//заполненный случайными числами из промежутка [minValue, maxValue].
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];// Создаем в памяти массив целых чисел с размером size
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+//Возвращает замену элементов массива arr
+
+int [] GetReplace(int[] arr)
+{
+
+    for (int el = 0; el < arr.Length; el++)
+    {
+        arr[el] *= (-1);
+    }
+
+    return arr;
+
+}
+
+
