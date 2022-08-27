4ced65ee3900680b7fd440305a638e4d6a62af02
Irina041708 <q-w-i-n-86@rambler.ru>
2022-08-27 22:28:56 +0400

    Dz№6

---
+++ Dz№6/Zad№1/Program.cs
@@ -0,0 +1,63 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+
+Console.Write("Введите количество цифр в массиве, которые вы хотели бы ввести: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+int[] array = GetArray(rows, -600, 600);
+Console.Write("Получился массив значений: [");
+PrintArray(array);
+Console.Write("]");
+
+int countcount = ModArray(array);
+Console.WriteLine();
+Console.Write($"Количество положительных чисел, которые Вы ввели равно {countcount}");
+
+
+
+int[] GetArray(int m, int minValue, int maxValue)
+{
+    int[] result = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        result[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return result;
+}
+
+void PrintArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        Console.Write($"{inArray[i]} ");
+
+    }
+  
+}
+
+int ModArray(int[] inArray)
+{
+    int count = 0;
+    int[] result = new int[count];
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+       
+        if (inArray[i] > 0) count++;
+                    
+    }
+    return count;
+}
+
+
+
+
+
---
+++ Dz№6/Zad№2/Program.cs
@@ -0,0 +1,69 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////
+Console.Clear();
+
+int[] array = GetArray(4, -10, 10);
+
+Console.Write("Получился массив коэффициентов: [");
+PrintArray(array);
+Console.WriteLine("]");
+
+PrintArray2(array);
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+int[] GetArray(int m, int minValue, int maxValue)
+{
+    int[] result = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите коэффициент {i + 1}:  ");
+        result[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    return result;
+}
+
+void PrintArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        Console.Write($"{inArray[i]} ");
+
+    }
+
+}
+
+
+
+void PrintArray2(int[] inArray)
+{
+
+    
+        if (array[0] - array[2] != 0 && array[1] - array[3] != 0)
+        {
+            
+            double x = (((double)array[2] - (double)array[0])) * (-1) / (double)(array[3] - (double)array[1]);
+            double y = ((double)array[0] * (double)array[3] - (double)array[1] * (double)array[2]) / ((double)array[3] - (double)array[1]);
+           
+            Console.WriteLine($"Две прямые пересекаются в точке с кординатми x = {x} и y = {y}");
+        }
+            
+        else Console.WriteLine("Две прямые параллельны.Решения нет.");
+    
+
+}
+
+
+
+/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
